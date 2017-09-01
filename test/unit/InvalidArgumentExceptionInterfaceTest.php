0aa4901b40ebcbe2cdd04551ecedd6999cc87996
Anton Ukhanev <xedin.unknown@gmail.com>
2017-09-01 18:26:28 +0200

    Replaced next version placeholder in tests

--- test/unit/InvalidArgumentExceptionInterfaceTest.php
+++ test/unit/InvalidArgumentExceptionInterfaceTest.php
@@ -7,21 +7,21 @@ use Xpmock\TestCase;
 /**
  * Tests {@see \Dhii\Exception\InvalidArgumentExceptionInterface}.
  *
- * @since [*next-version*]
+ * @since 0.1
  */
 class InvalidArgumentExceptionInterfaceTest extends TestCase
 {
     /**
      * The name of the test subject.
      *
-     * @since [*next-version*]
+     * @since 0.1
      */
     const TEST_SUBJECT_CLASSNAME = 'Dhii\Exception\InvalidArgumentExceptionInterface';
 
     /**
      * Creates a new instance of the test subject.
      *
-     * @since [*next-version*]
+     * @since 0.1
      *
      * @return \Dhii\Exception\InvalidArgumentExceptionInterface
      */
@@ -45,7 +45,7 @@ class InvalidArgumentExceptionInterfaceTest extends TestCase
     /**
      * Tests whether a valid instance of the test subject can be created.
      *
-     * @since [*next-version*]
+     * @since 0.1
      */
     public function testCanBeCreated()
     {
--- test/unit/ThrowableInterfaceTest.php
+++ test/unit/ThrowableInterfaceTest.php
@@ -7,21 +7,21 @@ use Xpmock\TestCase;
 /**
  * Tests {@see \Dhii\Exception\ThrowableInterface}.
  *
- * @since [*next-version*]
+ * @since 0.1
  */
 class ThrowableInterfaceTest extends TestCase
 {
     /**
      * The name of the test subject.
      *
-     * @since [*next-version*]
+     * @since 0.1
      */
     const TEST_SUBJECT_CLASSNAME = 'Dhii\Exception\ThrowableInterface';
 
     /**
      * Creates a new instance of the test subject.
      *
-     * @since [*next-version*]
+     * @since 0.1
      *
      * @return \Dhii\Exception\ThrowableInterface
      */
@@ -44,7 +44,7 @@ class ThrowableInterfaceTest extends TestCase
     /**
      * Tests whether a valid instance of the test subject can be created.
      *
-     * @since [*next-version*]
+     * @since 0.1
      */
     public function testCanBeCreated()
     {

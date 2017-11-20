dadca8325d61c422ebf05dd222a75393df48269a
Anton Ukhanev <xedin.unknown@gmail.com>
2017-11-20 08:45:52 +0100

    Added `BadValueExceptionInterface`

---
+++ src/BadValueExceptionInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dhii\Exception;
+
+use Dhii\Data\ValueAwareInterface;
+
+/**
+ * An exception that occurs in relation to a wrong, invalid, or erroneous value.
+ *
+ * @since [*next-version*]
+ */
+interface BadValueExceptionInterface extends ValueAwareInterface
+{
+}

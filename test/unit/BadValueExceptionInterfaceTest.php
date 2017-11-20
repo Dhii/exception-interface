43155c3226dc1fbd557b499aace6885c0d9bde0c
Anton Ukhanev <xedin.unknown@gmail.com>
2017-11-20 08:52:35 +0100

    Tightened test

--- test/unit/BadValueExceptionInterfaceTest.php
+++ test/unit/BadValueExceptionInterfaceTest.php
@@ -53,6 +53,7 @@ class BadValueExceptionInterfaceTest extends TestCase
         $subject = $this->createInstance();
 
         $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject, 'A valid instance of the subject could not be created');
+        $this->assertInstanceOf('Dhii\Exception\ThrowableInterface', $subject, 'Subject does not extend required interface');
         $this->assertInstanceOf('Dhii\Data\ValueAwareInterface', $subject, 'Subject does not extend required interface');
     }
 }

024134688f580af773246909ad1c93b5cc00e38b
Anton Ukhanev <xedin.unknown@gmail.com>
2017-12-28 00:12:12 +0100

    Added `OutOfRangeExceptionInterface`

---
+++ src/OutOfRangeExceptionInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dhii\Exception;
+
+/**
+ * Represents an exception that signals that a value is not within required parameters.
+ *
+ * @since [*next-version*]
+ */
+interface OutOfRangeExceptionInterface extends BadSubjectExceptionInterface
+{
+}

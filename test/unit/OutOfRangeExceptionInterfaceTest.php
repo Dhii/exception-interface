0c4a07de06e334153a90810712457f1bb6ac34b6
Anton Ukhanev <xedin.unknown@gmail.com>
2017-12-28 00:11:52 +0100

    Added test for `OutOfRangeExceptionInterface`

---
+++ test/unit/OutOfRangeExceptionInterfaceTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Dhii\Exception\UnitTest;
+
+use Xpmock\TestCase;
+use Dhii\Exception\OutOfRangeExceptionInterface as TestSubject;
+
+/**
+ * Tests {@see \Dhii\Exception\OutOfRangeExceptionInterface}.
+ *
+ * @since [*next-version*]
+ */
+class OutOfRangeExceptionInterfaceTest extends TestCase
+{
+    /**
+     * The name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\Exception\OutOfRangeExceptionInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return TestSubject
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+                ->getMessage()
+                ->getCode()
+                ->getFile()
+                ->getLine()
+                ->getTrace()
+                ->getTraceAsString()
+                ->getPrevious()
+                ->__toString()
+
+                ->getSubject()
+                ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject, 'A valid instance of the subject could not be created');
+        $this->assertInstanceOf('Dhii\Exception\BadSubjectExceptionInterface', $subject, 'Subject does not implement required interface');
+    }
+}

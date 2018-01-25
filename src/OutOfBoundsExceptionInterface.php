8a03130555b6d841c20e88cad5cb750449559639
Anton Ukhanev <xedin.unknown@gmail.com>
2018-01-25 21:16:23 +0100

    Added `OutOfBoundsExceptionInterface`
    This fixes #7.

---
+++ src/OutOfBoundsExceptionInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dhii\Exception;
+
+/**
+ * Represents an exception that signals that a value is requested from a set that does not have the value.
+ *
+ * @since [*next-version*]
+ */
+interface OutOfBoundsExceptionInterface extends BadSubjectExceptionInterface
+{
+}

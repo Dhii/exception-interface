f1a91625b5d080bc67b14376bfcf9e61a93febbe
Anton Ukhanev <xedin.unknown@gmail.com>
2018-02-19 22:36:48 +0100

    Added `InternalExceptionInterface`
    Fixes #13.

---
+++ src/InternalExceptionInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dhii\Exception;
+
+/**
+ * Represents an exception that is not caused by consumer actions.
+ *
+ * @since [*next-version*]
+ */
+interface InternalExceptionInterface extends ThrowableInterface
+{
+}

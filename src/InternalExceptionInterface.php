2e2e8d4e0170fafe5a6b5eadc8b83c95889103b4
Anton Ukhanev <xedin.unknown@gmail.com>
2018-02-23 14:05:45 +0100

    Corrected typo

--- src/InternalExceptionInterface.php
+++ src/InternalExceptionInterface.php
@@ -9,7 +9,7 @@ use Exception as RootException;
  * Represents an exception that is not caused by consumer actions.
  *
  * If this exception is thrown, it means that the cause of the problem
- * is not relates to any parameter passed to the unit that was invoked.
+ * is not related to any parameter passed to the unit that was invoked.
  *
  * Exceptions of this type guarantee an inner exceptions, so that consumers
  * can have a way of determining the cause of the problem.
